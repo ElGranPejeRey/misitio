fc0331fbb8aa77152a9ef316c48ebb1b89398662
ElGranPejeRey <erickbarriosarias30@gmail.com>
2025-08-12 09:31:19 -0400

    mi sitio

---
+++ js/menu.js
@@ -0,0 +1,6 @@
+const hamburguesa = document.getElementById("hamburguesa");
+const navbar2 = document.getElementById("navbar2");
+
+hamburguesa.addEventListener("click", () =>{
+    navbar2.classList.toggle("active")
+})
